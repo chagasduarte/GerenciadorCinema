1fcfda88403974d92387195105213c0d10930fae
chagasduarte <70854311+chagasduarte@users.noreply.github.com>
2022-11-12 20:28:32 -0300

    Adicionado Testes Unitários para Sessões

---
+++ CinemaTest/SessaoTestes.cs
@@ -0,0 +1,37 @@
+﻿using Cinema.Controllers.ModelsController;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTest
+{
+    public class SessaoTestes : IClassFixture<WebApplicationFactory<SessaoModelsController>>
+    {
+        private readonly WebApplicationFactory<SessaoModelsController> _factore;
+        public SessaoTestes(WebApplicationFactory<SessaoModelsController> factory)
+        {
+            _factore = factory;
+        }
+
+
+        [Theory]
+        [InlineData("SessaoModels/Index")]
+        [InlineData("SessaoModels/Create")]
+        [InlineData("SessaoModels/Delete")]
+        [InlineData("SessaoModels/Details")]
+        [InlineData("SessaoModels/Edit")]
+
+        public async Task TestAllPages(string url)
+        {
+            var client = _factore.CreateClient();
+
+            var response = await client.GetAsync(url);
+            int code = (int)response.StatusCode;
+            Assert.Equal(200, code);
+        }
+    }
+}
+
