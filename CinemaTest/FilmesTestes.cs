8f290c317670a556ffd6f2733d4be1c2bf7f7135
chagasduarte <70854311+chagasduarte@users.noreply.github.com>
2022-11-12 20:13:14 -0300

    Criado Teste Unitário para Filmes

---
+++ CinemaTest/FilmesTestes.cs
@@ -0,0 +1,37 @@
+﻿using Cinema.Controllers.ModelsController;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace CinemaTest
+{
+    public class FilmesTestes : IClassFixture<WebApplicationFactory<FilmesModelsController>>
+    {
+        private readonly WebApplicationFactory<FilmesModelsController> _factore;
+        public FilmesTestes(WebApplicationFactory<FilmesModelsController> factory)
+        {
+            _factore = factory;
+        }
+
+
+        [Theory]
+        [InlineData("FilmesModels/Index")]
+        [InlineData("FilmesModels/Create")]
+        [InlineData("FilmesModels/Delete")]
+        [InlineData("FilmesModels/Details")]
+        [InlineData("FilmesModels/Edit")]
+
+        public async Task TestAllPages(string url)
+        {
+            var client = _factore.CreateClient();
+
+            var response = await client.GetAsync(url);
+            int code = (int)response.StatusCode;
+            Assert.Equal(200, code);
+        }
+    }
+}
